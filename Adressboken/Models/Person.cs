427d54325a7ac925d88ce0768457a0b7827a74bb
MalinGr <112574619+MalinGr@users.noreply.github.com>
2023-08-14 11:39:06 +0200

    Tagit bort ?

--- Adressboken/Models/Person.cs
+++ Adressboken/Models/Person.cs
@@ -5,11 +5,11 @@ public class Person
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
-    public string Förnamn { get; set; }
-    public string Efternamn { get; set; }
-    public string Adress { get; set; }
-    public string Telefonnummer { get; set; }
+    public string? Förnamn { get; set; }
+    public string? Efternamn { get; set; }
+    public string? Adress { get; set; }
+    public string? Telefonnummer { get; set; }
 }
 
