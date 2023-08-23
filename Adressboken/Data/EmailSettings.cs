fe2ddaf5a1de8978c505fd9bf769a41497a938ff
[Martin Sandung] <[martin.sandung@gafe.molndal.se]>
2023-08-23 11:23:52 +0200

    bytt lösen igen

--- Adressboken/Data/EmailSettings.cs
+++ Adressboken/Data/EmailSettings.cs
@@ -8,7 +8,7 @@ namespace Adressboken.Data
         public string Host { get; set; } = "smtp.office365.com";
         public int Port { get; set; } = 587;
         public string UserName { get; set; } = "Bengts.BilService@hotmail.com";
-        public string Password { get; set; } = "bengt112";
+        public string Password { get; set; } = "bengt112112";
         public string SenderEmail { get; set; } = "Bengts.BilService@hotmail.com";
         public string SenderName { get; set; } = "Bengts BilService";
     }
