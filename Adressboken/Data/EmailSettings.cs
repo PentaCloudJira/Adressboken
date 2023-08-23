8e87c5f5457605419a699c89eab7b0e17f95d360
[Martin Sandung] <[martin.sandung@gafe.molndal.se]>
2023-08-23 11:42:16 +0200

    Bytt mail

--- Adressboken/Data/EmailSettings.cs
+++ Adressboken/Data/EmailSettings.cs
@@ -7,9 +7,9 @@ namespace Adressboken.Data
     {
         public string Host { get; set; } = "smtp.office365.com";
         public int Port { get; set; } = 587;
-        public string UserName { get; set; } = "Bengts.BilService@hotmail.com";
+        public string UserName { get; set; } = "bengts.bilservice2@hotmail.com";
         public string Password { get; set; } = "bengt112112";
-        public string SenderEmail { get; set; } = "Bengts.BilService@hotmail.com";
+        public string SenderEmail { get; set; } = "bengts.bilservice2@hotmail.com";
         public string SenderName { get; set; } = "Bengts BilService";
     }
 }
